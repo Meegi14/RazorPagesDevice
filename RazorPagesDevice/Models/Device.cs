84740c7ded690e9e75b9602a00bf558c8cde27c2
Magdalena Doderovic <magdalena.doderovic@gmail.com>
2018-12-18 16:40:33 +0100

    Add project files.

---
+++ RazorPagesDevice/Models/Device.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesDevice.Models
+{
+    public class Device
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+       
+        
+        public string DevicesTypes { get; set; }
+        public string DeviceTypeProperties { get; set; }
+        public int DevicesTypeValues { get; set; }
+    }   
+    
+
+}
